dccd927c99fdce8ffa258d74149e5e2a3b3dab43
cheeto <cheetowannashine@gmail.com>
2019-09-27 10:30:16 +0800

    对于项目配置的说明，备注补充

--- electron/db.js
+++ electron/db.js
@@ -1,11 +1,16 @@
-import { app } from 'electron'
+import {app} from 'electron'
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
-const adapter = new FileSync(app.getPath('home')+'/.db.json')
+const adapter = new FileSync(app.getPath('home') + '/.db.json')
 const db = low(adapter)
-db.defaults({ app_setting:{volume: 0.5},setting:{position:{}} })
-  .write()
-
+db.defaults({
+    app_setting: {
+        volume: 0.5
+    },
+    setting: {
+        position: {}
+    }
+}).write()
 
 global.db = db
 export default db
--- electron_webpack.js
+++ electron_webpack.js
@@ -1,5 +1,14 @@
 const path = require('path')
-
+/**
+ * Q：为什么要单独写这个 webpack 配置文件？
+ * A：electron开发时存在两个进程 main 和 renderer
+ * renderer 在开发时使用 create-react-app 也就是 webpack-dev-server 所生成的文件 （localhost：xxxx）打包之后为静态文件
+ * main 在官方文档里指出只要在 package.json 中的 main 字段写上对应的文件即可
+ * 但是 electron 不支持 es6 import 语法 https://stackoverflow.com/questions/35374353/es6-syntax-import-electron-require
+ * 对于数据库引入及扩展操作不是很方便
+ * 所以这边使用 webpack 将本身可以直接做文件入口的 electron/index.js 文件进行一次打包
+ * 同时 package.json 中的 main 填入打包之后的文件 即可支持 es6 语法
+ */
 module.exports = {
   mode: process.env.NODE_ENV == 'development' ? 'development' : 'production',
   entry: {
